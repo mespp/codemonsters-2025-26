2185d209f25a9fc64e16a26154a4990774aa4fcc
mespp <62350991+mespp@users.noreply.github.com>
2025-10-01 19:32:58 +0200

    Initial commit

---
+++ HolaMundo/src/LeerTeclado.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class LeerTeclado {
+    public static void main(String[] args) {
+        System.out.print("¿Cuál es tu nombre?: ");
+        Scanner sc = new Scanner(System.in);
+        String nombre = sc.nextLine();
+        System.out.println("¡Hola " + nombre + "!");
+    }
+}
