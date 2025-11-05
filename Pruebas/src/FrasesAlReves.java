48f0fbddf770bb06e1efcfdaf0ce7f5185ac9a92
mespp <62350991+mespp@users.noreply.github.com>
2025-11-05 19:31:04 +0100

    otro día más

---
+++ Pruebas/src/FrasesAlReves.java
@@ -0,0 +1,9 @@
+public class FrasesAlReves {
+
+    public static void main(String[] args) {
+        String texto = "esta es una prueba";
+        for (int i = 0; i < texto.length(); i++) {
+            char c = texto.charAt(i);
+        }
+    }
+}
---
+++ Pruebas/src/NumMayor.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class NumMayor {
+
+    public static void main(String[] args) {
+        List<Integer> numeros = List.of(3, 7, -1, 12, 11, 11);
+
+        Integer num_mayor = -2147483648;
+        for (int num: numeros) {
+            if (num > num_mayor) {
+                num_mayor = num;
+            }
+        }
+        System.out.println(num_mayor);
+    }
+}
