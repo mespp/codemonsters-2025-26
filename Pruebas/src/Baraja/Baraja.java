cb93e30b985d861736ea7a325d96832eb451199c
mespp <62350991+mespp@users.noreply.github.com>
2026-01-28 20:22:16 +0100

    ciertos arreglos a la puta mierda de arreglos de Guille

--- Pruebas/src/Baraja/Baraja.java
+++ Pruebas/src/Baraja/Baraja.java
@@ -10,10 +10,8 @@ public class Baraja {
     private List<Carta> mazo = new ArrayList();
     private String[] palos = {"oros", "bastos", "copas", "espadas"};
     private int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    // private List<Carta> mazoBackup = mazo;
 
     public void inicializar() {
-        // TODO: eliminar el mazo y poner las 40 cartas iniciales ordenadas
         eliminarTodasLasCartas();
         for (String p: palos) {
             for (int n: numeros) {
@@ -21,7 +19,6 @@ public class Baraja {
                  mazo.add(carta);
             }
         }
-        // System.out.println(mazo);
     }
 
     public void retirarCarta() {
@@ -48,7 +45,6 @@ public class Baraja {
     }
 
     public void listarTodasLasCartas() {
-        // TODO: listar las cartas del mazo
         for (Carta carta : mazo) {
             System.out.println(carta);
         }
@@ -59,8 +55,6 @@ public class Baraja {
     }
 
     public void barajar() {
-        // TODO: desordenar las cartas que contiene el mazo
         Collections.shuffle(mazo);
-        // System.out.println(mazo);
     }
 }
