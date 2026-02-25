4e1095efd2861901168e23020370f5a235073c33
mespp <62350991+mespp@users.noreply.github.com>
2026-02-25 19:33:27 +0100

    las letras y tal salen
    ahora voy a probar si moviéndolo todo no lo jodo jajaja

---
+++ SpaceInvadersLibGDX/assets/README.md
@@ -0,0 +1,6 @@
+- space_invaders.ttf
+  - https://github.com/leerob/space-invaders/blob/master/fonts/space_invaders.ttf
+
+- spaceInvadersSpriteSheed.png
+  - https://wallpapers.com/png/space-invaders-sprite-sheet-ik6vbjb7wy2pt9cj.html
+  - https://www.spriters-resource.com/arcade/spaceinv/asset/115520
--- SpaceInvadersLibGDX/core/src/main/java/game/mespp/spaceinvaderslibgdx/Main.java
+++ SpaceInvadersLibGDX/core/src/main/java/game/mespp/spaceinvaderslibgdx/Main.java
@@ -1,32 +1,47 @@
 package game.mespp.spaceinvaderslibgdx;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    // private Texture image;
+    private BitmapFont fontBig, fontRegular;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        FreeTypeFontGenerator fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/space_invaders.ttf"));
+        FreeTypeFontParameter fontParameter = new FreeTypeFontParameter();
+        fontParameter.color = Color.WHITE;
+        fontParameter.size = 28;
+        fontBig = fontGenerator.generateFont(fontParameter);
+        fontParameter.size = 12;
+        fontRegular = fontGenerator.generateFont(fontParameter);
+        fontGenerator.dispose();
+        //image = new Texture("libgdx.png");
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        ScreenUtils.clear(0f, 0f, 0f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        fontBig.draw(batch, "Space Invaders", 200, 50);
+        fontRegular.draw(batch, "press space to start", 200, 400);
+        //batch.draw(image, 140, 210);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        //image.dispose();
     }
 }
